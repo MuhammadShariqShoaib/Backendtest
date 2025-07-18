8e204be70f003719345890fa64f30aa99966b0e8
muhammad shariq <rajashariq435@gmail.com>
2025-07-18 18:06:54 +0500

    first commit

--- server.js
+++ server.js
@@ -13,10 +13,12 @@ app.use(cors());
 app.use("/api/students", studentRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI,{
-      useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.log(err));
-
+mongoose
+  .connect(process.env.MONGO_URI,{
+    
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(5000, () => console.log("Server running on port 5000"));
+  })
+  .catch((err) => console.log(err));
