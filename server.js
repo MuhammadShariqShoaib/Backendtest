b9fea19ea35d763264e9a5a81ddb0cb15737dbc1
muhammad shariq <rajashariq435@gmail.com>
2025-07-18 18:16:11 +0500

    first commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const studentRoutes = require("./routes/studentRoutes");
-
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cors());
