a4fa667f38a5dfb58c0420d7b1458ce6ba6f7d29
muhammad shariq <rajashariq435@gmail.com>
2025-07-18 17:15:56 +0500

    first commit

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 console.log("hellooooooooooooooooooooooooooooooooooooooooo",process.env.MONGO_URI);
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI,{
+      useNewUrlParser: true,
+  useUnifiedTopology: true
+})
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
 
